04bf0aa4964fd406453fbc70dfa91e6a1f14b15d
Nick Rodriquez <nicholasrodriquez@gmail.com>
2021-04-21 10:52:22 -0700

    Bridgecrew project add for GCP

---
+++ main.tf
@@ -0,0 +1,5 @@
+module "bridgecrew-read" {
+  source                     = "bridgecrewio/bridgecrew-gcp-read-only/google"
+  org_name               = "nicholasrodr"
+  bridgecrew_token = "9dfc959f-0f7e-5be1-9f01-c9925b694468"
+}
