dbd6634fc3614323fb82b73982f6bdce83190985
Nick Rodriquez <nicholasrodriquez@gmail.com>
2021-04-21 20:37:06 -0700

    updated

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 # project     = "test-project-298705"
 # }
 
-module "bridgecrew-read" {
-  source                     = "bridgecrewio/bridgecrew-gcp-read-only/google"
-  org_name               = "nicholasrodr"
-  bridgecrew_token = "9dfc959f-0f7e-5be1-9f01-c9925b694468"
-}
+# module "bridgecrew-read" {
+# source                     = "bridgecrewio/bridgecrew-gcp-read-only/google"
+#  org_name               = "nicholasrodr"
+#  bridgecrew_token = "9dfc959f-0f7e-5be1-9f01-c9925b694468"
+#}
